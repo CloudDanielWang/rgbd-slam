2f9eff4c4a1a79da337d7f7efc67cbfb7f1cee8a
CloudDanielWang <wzytjucloud@gmail.com>
2017-04-17 21:14:24 +0800

    修改g2o头文件于slambase.h头文件中

--- src/orb-slam-rgbd.cpp
+++ src/orb-slam-rgbd.cpp
@@ -1,16 +1,7 @@
 
 #include "slamBase.h"
 
-#include <g2o/types/slam3d/types_slam3d.h>
-#include <g2o/core/sparse_optimizer.h>
-#include <g2o/core/block_solver.h>
-#include <g2o/core/factory.h>
-#include <g2o/core/optimization_algorithm_factory.h>
-#include <g2o/core/optimization_algorithm_gauss_newton.h>
-#include <g2o/solvers/eigen/linear_solver_eigen.h>
-#include <g2o/core/robust_kernel.h>
-#include <g2o/core/robust_kernel_impl.h>
-#include <g2o/core/optimization_algorithm_levenberg.h>
+
 //using namespace cv;
 //using namespace pcl;
 using namespace std;
